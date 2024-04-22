cdc5e4d7a99765d26e2317ef73d41aa4f5484ece
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-04-22 19:02:18 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth-routes.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 // app.get("/",(req,res)=>{
@@ -23,6 +26,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 connectDB().then(()=>{
